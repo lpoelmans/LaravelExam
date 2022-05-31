6dbed9082f0b9f2a5005b235f70429d55c9db502
LiesbethPoelmans <liesbeth.poelmans@gmail.com>
2022-05-31 11:28:41 +0200

    Layout change

--- resources/views/myfav.blade.php
+++ resources/views/myfav.blade.php
@@ -1,27 +1,26 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <u><a href="search">Search your favorite movies</a></u>
-        </h2>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <u><a href="myfav">Your saved movies</a></u>
-        </h2>
-    </x-slot>
-    @isset ($savedmovies)
-        <div class="row mt-3">
-            @foreach ($savedmovies->savedmovie as $item )
-                @php
-                    $favs = Http::get('http://www.omdbapi.com/?apikey=ad34cbc1&i='.$item->imdbID);
-                    $favs = json_decode($favs);
-                @endphp
-                    <div class="container">
-                        <div class="card" style="width:25%"> 
-                            <h2 class="mt-3">{{$favs->Title}}</h2>
-                            <p>{{$favs->Type}} from {{$favs->Year}}</p>
-                            <img src="{{$favs->Poster}}" class="card-img-top rounded">
-                        </div>
-                    </div>
-            @endforeach  
+@extends('layouts.theme')
+    @section('content')
+    <div class="container mt-5">
+        <div class="container">
+            <h1 class="mb-5">Your Favorite Movies</h1>
         </div>
-    @endif 
-</x-app-layout>
+        <div class="container">
+            <p class="mt-3 mb-3"><a href="register" class="text-black"> Go to Dashboard </a></p>
+            @isset ($savedmovies)
+            <div class="row mt-3">
+                @foreach ($savedmovies->savedmovie as $item )
+                    @php
+                        $favs = Http::get('http://www.omdbapi.com/?apikey=ad34cbc1&i='.$item->imdbID);
+                        $favs = json_decode($favs);
+                    @endphp
+                    <div class="card" style="width:25%"> 
+                        <h2 class="mt-3">{{$favs->Title}}</h2>
+                        <p>{{$favs->Type}} from {{$favs->Year}}</p>
+                        <img src="{{$favs->Poster}}" class="card-img-top rounded">
+                    </div>
+                @endforeach  
+            </div>
+            @endif  
+        </div>
+    @endsection
+
